69dab7e0b4805ed8747a86d75570a3f459905d9e
Sur.en <surenkirakosyan.am@gmail.com>
2022-05-01 15:55:24 +0400

    Change credentials.json file path to /etc/opt

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ config :logger, :console, format: "[$level] $message\n"
 
 config :sarqx_reporter,
   server_host: "http://localhost:4000",
-  path_to_credential_file: "#{File.cwd!()}/etc/credentials.json",
+  path_to_credential_file: "#{File.cwd!()}/etc/opt/sarqx-reporter/credentials.json",
   secret: "14Ubf02J3f3T9erhP6xH095jG+t+HDVnBH5pbWx35JOKJg7OrYsLI4LJU8d1keVE",
   salt: "W28hwiH6xH095gG+t+HDVnbPVm2JrYsLI48d1qlOU",
   askpass: "#{File.cwd!()}/askpass.sh",
